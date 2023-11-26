150db076493480d449ae66b8bd6a30118114f852
joao-henrique10 <jhcalzavara@hotmail.com>
2023-11-26 13:27:36 -0300

    Add UPDATE Veiculo procedure

--- src/Trigger_Storage_Procedure.sql
+++ src/Trigger_Storage_Procedure.sql
@@ -47,3 +47,76 @@ BEGIN
         INSERT INTO Terrestre VALUES (_id, _sala, _nome, _vida, _numRodas, _combustivel);
     END IF;
 END $$;
+
+
+CREATE OR REPLACE PROCEDURE update_veiculo(_id INT, _sala INT, _nome VARCHAR(255), _vida INT, _combustivel INT, _numRodas SMALLINT, _propulsao INT, _maxAltitude INT)
+LANGUAGE plpgsql AS $$
+BEGIN
+    IF _maxAltitude IS NULL AND _propulsao IS NULL AND _numRodas IS NULL THEN
+        RAISE EXCEPTION 'Precisa ter uma variável específica NOT NULL';
+    ELSIF _maxAltitude IS NOT NULL AND _propulsao IS NOT NULL AND _numRodas IS NOT NULL THEN
+        RAISE EXCEPTION 'Precisa ter apenas uma variável específica NOT NULL';
+    ELSIF _maxAltitude IS NOT NULL AND _propulsao IS NOT NULL THEN 
+        RAISE EXCEPTION 'Precisa ter apenas uma variável específica NOT NULL';
+    ELSIF _maxAltitude IS NOT NULL AND _numRodas IS NOT NULL THEN 
+        RAISE EXCEPTION 'Precisa ter apenas uma variável específica NOT NULL';
+    ELSIF _propulsao IS NOT NULL AND _numRodas IS NOT NULL THEN
+        RAISE EXCEPTION 'Precisa ter apenas uma variável específica NOT NULL';
+
+    ELSIF _maxAltitude IS NOT NULL THEN
+        PERFORM * FROM Aquatico WHERE _id = Aquatico.idVeiculo;
+        IF FOUND THEN
+            DELETE FROM Aquatico WHERE _id = Aquatico.idVeiculo;
+            INSERT INTO Aereo VALUES (_id, _sala, _nome, _vida, _combustivel, _maxAltitude);
+        END IF;
+        PERFORM * FROM Terrestre WHERE _id = Terrestre.idVeiculo;
+        IF FOUND THEN
+            DELETE FROM Terrestre WHERE _id = Terrestre.idVeiculo;
+            INSERT INTO Aereo VALUES (_id, _sala, _nome, _vida, _combustivel, _maxAltitude);
+        END IF;
+        PERFORM * FROM Aereo WHERE _id = Aereo.idVeiculo;
+        IF FOUND THEN
+            UPDATE Aereo SET sala = _sala, nome = _nome, vida = _vida, combustivel = _combustivel, maxAltitude = _maxAltitude WHERE idVeiculo = _id;
+        ELSE 
+            RAISE EXCEPTION 'O id colocado não existe na tabela Aéreo';
+        END IF;
+
+    ELSIF _numRodas IS NOT NULL THEN
+        PERFORM * FROM Aquatico WHERE _id = Aquatico.idVeiculo;
+        IF FOUND THEN
+            DELETE FROM Aquatico WHERE _id = Aquatico.idVeiculo;
+            INSERT INTO Terrestre VALUES (_id, _sala, _nome, _vida, _numRodas, _combustivel);
+        END IF;
+        PERFORM * FROM Aereo WHERE _id = Aereo.idVeiculo;
+        IF FOUND THEN
+            DELETE FROM Aereo WHERE _id = Aereo.idVeiculo;
+            INSERT INTO Terrestre VALUES (_id, _sala, _nome, _vida, _numRodas, _combustivel);
+        END IF;
+        PERFORM * FROM Terrestre WHERE _id = Terrestre.idVeiculo;
+        IF FOUND THEN
+            UPDATE Terrestre SET sala = _sala, nome = _nome, vida = _vida, numRodas = _numRodas, combustivel = _combustivel WHERE idVeiculo = _id;
+        ELSE 
+            RAISE EXCEPTION 'O id colocado não existe na tabela Terrestre';
+        END IF;
+
+    ELSIF _propulsao IS NOT NULL THEN
+        PERFORM * FROM Terrestre WHERE _id = Terrestre.idVeiculo;
+        IF FOUND THEN
+            DELETE FROM Terrestre WHERE _id = Terrestre.idVeiculo;
+            INSERT INTO Aquatico VALUES (_id, _sala, _nome, _vida, _combustivel, _propulsao);
+        END IF;
+        PERFORM * FROM Aereo WHERE _id = Aereo.idVeiculo;
+        IF FOUND THEN
+            DELETE FROM Aereo WHERE _id = Aereo.idVeiculo;
+            INSERT INTO Aquatico VALUES (_id, _sala, _nome, _vida, _combustivel, _propulsao);
+        END IF;
+        PERFORM * FROM Aquatico WHERE _id = Aquatico.idVeiculo;
+        IF FOUND THEN
+            UPDATE Aquatico SET sala = _sala, nome = _nome, vida = _vida, combustivel = _combustivel, propulsao = _propulsao WHERE idVeiculo = _id;
+        ELSE 
+            RAISE EXCEPTION 'O id colocado não está na tabela Aquatico';
+        END IF;
+    END IF;
+END $$;
+
+
